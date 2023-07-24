a019c1c5bc725fdc1f28890b387b778d60fc57f1
JesusMtnez <jesusmartinez93@gmail.com>
2023-07-24 19:57:20 +0200

    add AccountRead model

---
+++ modules/fireflyiii4s/src/main/scala/es/jesusmtnez/ff/data/AccountRead.scala
@@ -0,0 +1,19 @@
+package es.jesusmtnez.ff.data
+
+import cats.syntax.all.*
+import io.circe.*
+
+final case class AccountRead(
+  `type`: String,
+  id: String,
+  attributes: Account
+)
+
+object AccountRead:
+  given Decoder[AccountRead] = new Decoder[AccountRead]:
+    final def apply(c: HCursor): Decoder.Result[AccountRead] =
+      (
+        c.downField("type").as[String],
+        c.downField("id").as[String],
+        c.downField("attributes").as[Account]
+      ).mapN(AccountRead.apply)
